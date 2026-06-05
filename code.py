edce815d00be4f1347e1646854442d195a6c1b50
Anirudh Bansal <aniru48b@gmail.com>
2026-06-05 22:41:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -83,7 +83,7 @@ if 'assigned_key' not in st.session_state:
 
 client = get_genai_client(st.session_state.assigned_key)
 collection = get_db_collection()
-ai_model = "gemini-3-flash"
+ai_model = "gemini-2.5-flash-lite"
 
 
 
